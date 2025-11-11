377a5ea279d8e0f31aa80c66fdb5543207147ffa
Amr <floxylak359@gmail.com>
2025-11-11 09:49:40 +0200

    Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -5,15 +5,15 @@
 using namespace std;
 //#write Instructor class here
 
+class Instructor : public Person {
+private:
+    int experienceYears;
+    string department;
+public:
+    Instructor(string name, int id, int experienceYears, string department);
+    void display();
 
-
-
-
-
-
-
-
-
-
+    ~Instructor();
+};
 
 #endif
