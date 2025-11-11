49c57495b15e5e8704652dd3a372d7e86a643979
Amr <floxylak359@gmail.com>
2025-11-11 09:48:51 +0200

    Course.h

--- Course.h
+++ Course.h
@@ -6,17 +6,20 @@
 using namespace std;
 //#write your code here
 
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
 
+public:
+    Course(string courseCode, string courseName, int maxStudents);
+    void addStudent(const Student& s);
+    void displayCourseInfo();
 
-
-
-
-
-
-
-
-
-
-
+    ~Course();
+};
 
 #endif
